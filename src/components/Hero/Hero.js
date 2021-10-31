64cdd115a68871e2173cffb49e2a0c3576f1904f
alaqsaka <aqsaabidin15@gmail.com>
2021-10-31 14:35:48 +0700

    chore: finishing hero section
    - Creating hero section
    - Changing background color
    - Changing font style
    - Adding background animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        19 years old guy, web development and <br />
+        technologies enthusiast. <br />
+        Currently working for myself and investing in my dreams.
+      </SectionTitle>
+      <SectionText>Based in Tangerang Selatan, Banten, Indonesia</SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- src/themes/default.js
+++ src/themes/default.js
@@ -1,22 +1,22 @@
 export default {
   // Temp fonts
   fonts: {
-    title: "Space Grotesk, sans-serif",
-    main: "Space Grotesk, sans-serif"
+    title: "Poppins, sans-serif",
+    main: "Poppins, sans-serif",
   },
   // Colors for layout
   colors: {
     primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
+    background1: " #141829",
     accent1: "hsl(34.9,98.6%,72.9%)",
     button: "hsl(205.1,100%,36.1%)",
     background2: "hsl(232.7,27.3%,23.7%)",
   },
   // Breakpoints for responsive design
   breakpoints: {
-    sm: 'screen and (max-width: 640px)',
-    md: 'screen and (max-width: 768px)',
-    lg: 'screen and (max-width: 1024px)',
-    xl: 'screen and (max-width: 1280px)'
+    sm: "screen and (max-width: 640px)",
+    md: "screen and (max-width: 768px)",
+    lg: "screen and (max-width: 1024px)",
+    xl: "screen and (max-width: 1280px)",
   },
-}
+};
