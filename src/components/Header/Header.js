d1775220de9d45503e09f38edb8810ce98598e4c
alaqsaka <aqsaabidin15@gmail.com>
2021-10-31 14:58:19 +0700

    chore: adding fade animation on header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ import {
   AiFillTwitterSquare,
 } from "react-icons/ai";
 import { DiCssdeck } from "react-icons/di";
+import { Fade } from "react-reveal";
 
 import {
   Container,
@@ -19,51 +20,53 @@ import {
 } from "./HeaderStyles";
 
 const Header = () => (
-  <Container>
-    <Div1>
-      <Link href="/">
-        <a
-          style={{
-            display: "flex",
-            alignItems: "center",
-            color: "white",
-            marginBottom: "20px",
-          }}
-          href=""
-        >
-          <DiCssdeck size="3rem" /> <Span>Portofolio</Span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href="#projects">
-          <NavLink>Projects</NavLink>
+  <Fade bottom duration={1500}>
+    <Container>
+      <Div1>
+        <Link href="/">
+          <a
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "white",
+              marginBottom: "20px",
+            }}
+            href=""
+          >
+            <DiCssdeck size="3rem" /> <Span>Portofolio</Span>
+          </a>
         </Link>
-      </li>
-      <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#about">
-          <NavLink>About</NavLink>
-        </Link>
-      </li>
-    </Div2>
-    <Div3>
-      <SocialIcons href="https://github.com">
-        <AiFillGithub size="3rem" />
-      </SocialIcons>
-      <SocialIcons href="https://linkedin.com">
-        <AiFillLinkedin size="3rem" />
-      </SocialIcons>
-      <SocialIcons href="https://twitter.com">
-        <AiFillTwitterSquare size="3rem" />
-      </SocialIcons>
-    </Div3>
-  </Container>
+      </Div1>
+      <Div2>
+        <li>
+          <Link href="#projects">
+            <NavLink>Projects</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href="#tech">
+            <NavLink>Technologies</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href="#about">
+            <NavLink>About</NavLink>
+          </Link>
+        </li>
+      </Div2>
+      <Div3>
+        <SocialIcons href="https://github.com">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://linkedin.com">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://twitter.com">
+          <AiFillTwitterSquare size="3rem" />
+        </SocialIcons>
+      </Div3>
+    </Container>
+  </Fade>
 );
 
 export default Header;
