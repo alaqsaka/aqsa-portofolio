e4836eed1aefc2acfdbb5e0d7b3804fde02d5358
alaqsaka <aqsaabidin15@gmail.com>
2021-11-01 16:39:39 +0700

    chore: editing data in accomplishment and editing styling

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  {
+    date: "July 2021",
+    text: "Menjadi Front-End Web Developer Expert by Dicoding Indonesia",
+  },
+  {
+    date: "May 2021",
+    text: "Belajar Membuat Aplikasi Back-End untuk Pemula by Dicoding Indonesia",
+  },
+  {
+    date: "May 2021",
+    text: "Belajar dasar pemrograman Javascript by Dicoding Indonesia",
+  },
+  {
+    date: "April 2021",
+    text: "Belajar Fundamental Front-End Web Development by Dicoding Indonesia",
+  },
+  {
+    date: "February 2021",
+    text: "Path Pengembangan Web(Node.js) and Kursus React by Progate",
+  },
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>My Certifications</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box ket={index}>
+          <BoxNum>{card.date}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
