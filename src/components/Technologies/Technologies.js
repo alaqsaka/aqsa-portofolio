9f2a3229f0c61f457b20950d30b75fb6a1a47a1f
alaqsaka <aqsaabidin15@gmail.com>
2021-11-01 16:20:50 +0700

    chore: adding fade effect on tech and timeline

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -19,47 +19,50 @@ import {
   ListParagraph,
   ListTitle,
 } from "./TechnologiesStyles";
+import { Fade } from "react-reveal";
 
 const Technologies = () => (
-  <Section id="tech">
-    <SectionDivider />
-    <br />
-    <SectionTitle>Technologies</SectionTitle>
-    <SectionText>
-      Currently focusing on studying web development, especially front-end
-      development, using some of these technologies
-    </SectionText>
-    <List>
-      <ListItem>
-        <DiReact size="3rem" />
-        <ListContainer>
-          <ListTitle>Front-End</ListTitle>
-          <ListParagraph>
-            Currently learning and building stuff with ReactJS
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <DiNodejs size="3rem" />
-        <ListContainer>
-          <ListTitle>Back-End</ListTitle>
-          <ListParagraph>
-            Experience with <br />
-            NodeJS and Databases, MYSQL.
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <DiBootstrap size="3rem" />
-        <ListContainer>
-          <ListTitle>Design</ListTitle>
-          <ListParagraph>
-            Experience with UI/UX Tools, Figma. Web styling, Bootstrap, CSS.
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-    </List>
-  </Section>
+  <Fade bottom>
+    <Section id="tech">
+      <SectionDivider />
+      <br />
+      <SectionTitle>Technologies</SectionTitle>
+      <SectionText>
+        Currently focusing on studying web development, especially front-end
+        development, using some of these technologies
+      </SectionText>
+      <List>
+        <ListItem>
+          <DiReact size="3rem" />
+          <ListContainer>
+            <ListTitle>Front-End</ListTitle>
+            <ListParagraph>
+              Currently learning and building stuff with ReactJS
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiNodejs size="3rem" />
+          <ListContainer>
+            <ListTitle>Back-End</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              NodeJS and Databases, MYSQL.
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiBootstrap size="3rem" />
+          <ListContainer>
+            <ListTitle>Design</ListTitle>
+            <ListParagraph>
+              Experience with UI/UX Tools, Figma. Web styling, Bootstrap, CSS.
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+      </List>
+    </Section>
+  </Fade>
 );
 
 export default Technologies;
